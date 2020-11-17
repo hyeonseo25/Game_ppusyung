009c4e13e08b2e271b84afb7de96de77ecb6dc2a
jimin0127 <ckdgh4662@naver.com>
2020-11-18 00:22:18 +0900

    클리어 패널 배경 수정 버튼 수정

--- Project/src/panels/ClearPanel.java
+++ Project/src/panels/ClearPanel.java
@@ -1,26 +1,46 @@
 package panels;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import util.DBConnection;
 
 public class ClearPanel extends JPanel{
 	private JButton replaybt;
 	private JButton rankbt;
-	private TextField name;
+	private JTextField name;
 	private JButton applybt;
 	private JLabel scoreLabel;
 	private String scoreText;
 	private int score; // 플레이어 점수
+	private ImageIcon ClearMessageImg = new ImageIcon("images/게임클리어.gif");
+	private Image ClearMessage = ClearMessageImg.getImage();
+	
+
+	
+	private ImageIcon backImg = new ImageIcon("images/클리어패널배경.png");
+	private Image back = backImg.getImage();
+	
+
+
+
+	
 	
 	
 	public String getScoreText() {
@@ -43,20 +63,45 @@ public class ClearPanel extends JPanel{
 		replaybt.addMouseListener((MouseListener) o);
 		add(replaybt);
 		
-		scoreLabel = new JLabel(getScoreText());
+		scoreLabel = new JLabel(Integer.toString(score));
 		scoreLabel.setLocation(500, 50);
 		add(scoreLabel);
 		
-		name = new TextField();
+		
+		
+		Font font=new Font("돋음", Font.BOLD, 35);
+		name = new JTextField("이름을 입력해주세요");
+		name.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+		name.setHorizontalAlignment(JTextField.CENTER);
+		
+		// hint 리스너
+		name.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent e) {
+			     name.setText("이름을 입력해주세요");			
+			}
+			@Override
+			public void focusGained(FocusEvent e) {
+		        	name.setText("");
+			}
+		});
 		name.setName("name");
-		name.setBounds(600, 600, 150, 150);
+		name.setFont(font);
+		name.setLocation(800, 790);
+		name.setSize(600, 50);
 		add(name);
 		
-		applybt = new JButton();
+		
+		applybt = new JButton(new ImageIcon("images/button/clearPanelBtn.png"));
 		applybt.setName("applyButton");
-		applybt.setText("확인" );
-		applybt.setBounds(500, 500, 200, 100);
+		applybt.setBorderPainted(false);
+		applybt.setFocusPainted(false);
+		applybt.setContentAreaFilled(false);
+		applybt.setBounds(1450, 710, 200, 200);
 		applybt.addMouseListener((MouseListener) o);
+		
+		
 		applybt.addActionListener(new ActionListener() {
 			
 			@Override
@@ -71,13 +116,21 @@ public class ClearPanel extends JPanel{
 		rankbt = new JButton();
 		rankbt.setName("RankingButton");
 		rankbt.setText("랭킹");
-		rankbt.setBounds(1000, 800, 200, 100);
+		rankbt.setBounds(100, 100, 200, 100);
 		rankbt.addMouseListener((MouseListener) o);
 		add(rankbt);
 		
-		
-		
-		setBackground(Color.YELLOW);
 	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+		g.drawImage(back, 0, 0, this);
+		
+		g.drawImage(ClearMessage, 800, 120, this);
+	}
+	
+	
 
 }
