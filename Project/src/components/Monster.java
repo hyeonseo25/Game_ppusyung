ab198a216a550a8d2f70f25c670eec173849870e
hyeonseo25 <s2019s30@e-mirim.hs.kr>
2020-11-21 23:58:27 +0900

    몬스터 필드 수정

--- Project/src/components/Monster.java
+++ Project/src/components/Monster.java
@@ -33,19 +33,19 @@ public class Monster {
 	
     public void createMonsters() { //¸Ş¼Òµå È£Ãâ ½Ã Monster °´Ã¼°¡ ¹è¿­¿¡ Ãß°¡
     	monsterList.clear();
-		monsterList.add(new MonsterThread(1200, 0, 100, "images/monsters/Áö··ÀÌ±«¹°.gif", player));
-	    monsterList.add(new GunMonster(2600, 600, 130, "images/monsters/¹°°É·¹±«¹°7x.gif", player));
-	    monsterList.add(new MonsterThread(3200, 0, 100, "images/monsters/½½¶óÀÓ±«¹°1.gif", player));
-	    monsterList.add(new MonsterThread(4100, 600, 100, "images/monsters/Ã¥±«¹°7x.gif", player));	
-	    monsterList.add(new MonsterThread(4500, 600, 100, "images/monsters/³¯°³±«¹°7x.gif", player));		
-	    monsterList.add(new MonsterThread(5600, 600, 100, "images/monsters/½½¶óÀÓ±«¹°1x4.gif", player));		
-	    monsterList.add(new MonsterThread(7500, 600, 100, "images/monsters/Ã¥±«¹°.gif", player));		
-	    monsterList.add(new MonsterThread(9600, 600, 100, "images/monsters/³ë¶õ»ö½½¶óÀÓ±«¹°1x4.gif", player));		
-	    monsterList.add(new MonsterThread(10000, 600, 100, "images/monsters/¹°°É·¹±«¹°7x.gif", player));		
-	    monsterList.add(new MonsterThread(8000, 600, 100, "images/monsters/Áö··ÀÌ±«¹°.gif", player));		
-	    monsterList.add(new MonsterThread(10600, 600, 100, "images/monsters/³ë¶õ»ö½½¶óÀÓ±«¹°.gif", player));		
-	    monsterList.add(new MonsterThread(12000, 600, 100, "images/monsters/¹°°É·¹±«¹°7x.gif", player));		
-	    monsterList.add(new MonsterThread(13000, 600, 100, "images/monsters/Áö··ÀÌ±«¹°.gif", player));
+		monsterList.add(new MonsterThread(1200, 450, 100, "images/monsters/Áö··ÀÌ±«¹°.gif", player));
+	    //monsterList.add(new GunMonster(1600, 400, 130, "images/monsters/¹°°É·¹±«¹°7x.gif", player));
+	    monsterList.add(new MonsterThread(1200, 450, 100, "images/monsters/½½¶óÀÓ±«¹°1.gif", player));
+	    monsterList.add(new MonsterThread(1300, 400, 100, "images/monsters/Ã¥±«¹°7x.gif", player));	
+	    monsterList.add(new MonsterThread(4500, 400, 100, "images/monsters/³¯°³±«¹°7x.gif", player));		
+	    monsterList.add(new MonsterThread(5600, 400, 100, "images/monsters/½½¶óÀÓ±«¹°1x4.gif", player));		
+	    monsterList.add(new MonsterThread(7500, 400, 100, "images/monsters/Ã¥±«¹°.gif", player));		
+	    monsterList.add(new MonsterThread(9600, 400, 100, "images/monsters/³ë¶õ»ö½½¶óÀÓ±«¹°1x4.gif", player));		
+	    monsterList.add(new MonsterThread(10000, 400, 100, "images/monsters/¹°°É·¹±«¹°7x.gif", player));		
+	    monsterList.add(new MonsterThread(8000, 400, 100, "images/monsters/Áö··ÀÌ±«¹°.gif", player));		
+	    monsterList.add(new MonsterThread(10600, 400, 100, "images/monsters/³ë¶õ»ö½½¶óÀÓ±«¹°.gif", player));		
+	    monsterList.add(new MonsterThread(12000, 400, 100, "images/monsters/¹°°É·¹±«¹°7x.gif", player));		
+	    monsterList.add(new MonsterThread(13000, 400, 100, "images/monsters/Áö··ÀÌ±«¹°.gif", player));
         for (int i = 0; i < monsterList.size(); i++) {
         	monsterList.get(i).start();
         }
